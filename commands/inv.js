960621c6489a0abd5f7d7ae457d56acd2230a78d
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-03-19 19:47:25 -0300

    Update inv.js
    Troca de Link de invite pra Raphy Nova

--- commands/inv.js
+++ commands/inv.js
@@ -8,7 +8,7 @@ message.delete()
                 .addFields(
                         {
                             name: 'Aqui está o link para me adicionar no seu Servidor',
-                            value: 'https://discord.com/oauth2/authorize?client_id=763072871597604874&scope=bot&permissions=8'
+                            value: 'https://discord.com/api/oauth2/authorize?client_id=821471191578574888&permissions=8&scope=bot'
                         })
       await message.channel.send(embed)
 }

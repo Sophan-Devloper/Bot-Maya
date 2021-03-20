0d846316a9e25f326c3c9017d2af36321de70443
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-03-20 16:26:36 -0300

    Update removerole.js
    fixed and updated

--- moderation/removerole.js
+++ moderation/removerole.js
@@ -1,27 +1,23 @@
 const Discord = require('discord.js')
 
 module.exports.run = async (bot, message, args) => {
-message.delete()
+    message.delete()
 
-let perms = message.member.hasPermission("MANAGE_ROLES")
-if(!perms) return message.channel.send("Você não tem permissão suficiente pra este comando, que tal chamar um Mod ou Adm?").then(msg => msg.delete({timeout: 4000}))
+    let perms = message.member.hasPermission("MANAGE_ROLES")
+    if (!perms) return message.channel.send("Você não tem permissão suficiente pra esse comando, que tal chamar um Mod ou Adm?").then(msg => msg.delete({ timeout: 3000 }))
 
-let user = message.mentions.members.first()
-if(!user) return message.channel.send("Tenta assim -> `-removerole @user @cargo`").then(msg => msg.delete({timeout: 5000}))
+    let user = message.mentions.members.first()
+    if (!user) return message.channel.send("Tenta assim -> `-removerole @user @cargo`").then(msg => msg.delete({ timeout: 3000 }))
 
-let role = message.mentions.roles.first()
-if(!role) return message.channel.send("Tenta assim -> `-removerole @user @cargo`").then(msg => msg.delete({timeout: 5000}))
+    let role = message.mentions.roles.first()
+    if (!role) return message.channel.send("Qual é o cargo? Você não me disse.").then(msg => msg.delete({ timeout: 3000 }))
 
-let avatar = user.user.displayAvatarURL({format: 'png'})
+    if (!user.roles.cache.has(role.id)) {
+        return message.channel.send(`${user.user.username} não possui esse cargo.`).then(msg => msg.delete({ timeout: 3000 }))
+    }
 
-user.roles.remove(role)
-message.channel.send(`O Cargo ${role} foi removido de ${user} com sucesso!`).then(msg => msg.delete({timeout: 6000}))
-}
+    let avatar = user.user.displayAvatarURL({ format: 'png' })
 
-module.exports.help = {
-    name: "removerole",
-    aliases: ['rmrole'],
-    description: "Remove roles to someone",
-    usage: "removerole user role",
-    category: "settings, administration"
+    user.roles.remove(role)
+    message.channel.send(`${user.user.username} teve o cargo ${role} removido com sucesso!`).then(msg => msg.delete({ timeout: 6000 }))
 }

253bfde00321de347256d0eb66c797bc3e59effd
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-03-20 12:03:38 -0300

    Update mute.js
    Consertado

--- moderation/mute.js
+++ moderation/mute.js
@@ -34,48 +34,73 @@ module.exports.run = async (bot, message, args) => {
                MANAGE_ROLES: false
             })
          })
+      } catch (error) {
+         console.log(error)
+      }
+      try {
+         const role7 = message.guild.roles.cache.find(role => role.name === 'Muted')
 
-         const rol = ('Muted')
+         const roleembedcreate = new Discord.MessageEmbed()
+            .setColor('#DCDCDC')
+            .setTitle('Detalhes Especificos')
+            .setDescription(`Enviando detalhes mais especificos para ${message.author} e para o dono do servidor...`)
+
          message.channel.send('Eu não encontrei nenhum cargo de Mute eficiente.').then(msg => msg.delete({ timeout: 5000 })).then(msg => msg.channel.send(
             'Criando cargo...'
          )).then(msg => msg.delete({ timeout: 5000 })).then(msg => msg.channel.send(
-            `O cargo ${rol} foi criado com sucesso!`
+            `O cargo ${role7} foi criado com sucesso!`
          )).then(msg => msg.delete({ timeout: 3000 })).then(msg => msg.channel.send(
             'Configurando o cargo no servidor...'
          )).then(msg => msg.delete({ timeout: 5000 })).then(msg => msg.channel.send(
-            `Negando permissões do cargo ${rol}...`
+            `Negando permissões do cargo ${role7}...`
          )).then(msg => msg.delete({ timeout: 5000 })).then(msg => msg.channel.send(
             'Permissões Negadas:'
-         )).then(msg => msg.delete({ timeout: 2000 })).then(msg => msg.channel.send(
+         )).then(msg => msg.delete({ timeout: 1000 })).then(msg => msg.channel.send(
             'Permissões Negadas:\n`Enviar mensagens de texto`'
-         )).then(msg => msg.delete({ timeout: 2000 })).then(msg => msg.channel.send(
+         )).then(msg => msg.delete({ timeout: 1000 })).then(msg => msg.channel.send(
             'Permissões Negadas:\n`Enviar mensagens de texto`\n`Enviar mensagem TTS`'
-         )).then(msg => msg.delete({ timeout: 2000 })).then(msg => msg.channel.send(
+         )).then(msg => msg.delete({ timeout: 1000 })).then(msg => msg.channel.send(
             'Permissões Negadas:\n`Enviar mensagens de texto`\n`Enviar mensagem TTS`\n`Conectar e falar em canais de voz`'
-         )).then(msg => msg.delete({ timeout: 2000 })).then(msg => msg.channel.send(
+         )).then(msg => msg.delete({ timeout: 1000 })).then(msg => msg.channel.send(
             'Permissões Negadas:\n`Enviar mensagens de texto`\n`Enviar mensagem TTS`\n`Conectar e falar em canais de voz`\n`Reagir em mensagens com emojis`'
-         )).then(msg => msg.delete({ timeout: 2000 })).then(msg => msg.channel.send(
+         )).then(msg => msg.delete({ timeout: 1000 })).then(msg => msg.channel.send(
             'Permissões Negadas:\n`Enviar mensagens de texto`\n`Enviar mensagem TTS`\n`Conectar e falar em canais de voz`\n`Reagir em mensagens com emojis`\n`Manusear mensagens e cargos`'
-         )).then(msg => msg.delete({ timeout: 4000 })).then(msg => msg.channel.send(
+         )).then(msg => msg.delete({ timeout: 6000 })).then(msg => msg.channel.send(
+            'Verificando erros na configuranção...'
+         )).then(msg => msg.delete({ timeout: 2000 })).then(msg => msg.channel.send(
+            'Nenhum erro encontrado.'
+         )).then(msg => msg.delete({ timeout: 1500 })).then(msg => msg.channel.send(
+            roleembedcreate
+         )).then(msg => msg.delete({ timeout: 8000 })).then(msg => msg.channel.send(
             `Cargo criado e configurado com sucesso!`
-         )).then(msg => msg.channel.send(
-            `Enviando detalhes mais especificos para ${message.author} e para o dono do servidor com todos os detalhes do Comando Mute...`
-         )).then(msg => msg.delete({ timeout: 8000 })).then(msg => message.guild.owner.send && message.author.send(
-            `EMBED DO MUTE DETALHADA`
          ))
       } catch (error) {
          console.log(error)
       }
+
+      function msgembed() {
+         if (message.guild.owner.id === message.author.id) {
+            message.author.send(`EMBED DO MUTE DETALHADA`)
+         }
+         if (message.guild.owner.id != message.author.id) {
+            message.author.send(`EMBED DO MUTE DETALHADA`)
+            message.guild.owner.send(`EMBED DO MUTE DETALHADA`)
+         }
+      }
+      setTimeout(msgembed, 44000)
    }
    if (!role) return
 
    const member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
    if (!member)
-      return message.channel.send("Tenta assim `-mute @user 10s/m/h razão`").then(msg => msg.delete({ timeout: 10000 }))
+      return message.channel.send("Comando Mute\n`-mute @user 10s/m/h razão`").then(msg => msg.delete({ timeout: 10000 }))
 
    const Raphy = member.id === '821471191578574888'
    if (Raphy)
       return message.channel.send("Eu não vou mutar eu mesma, sorry :/")
+
+   if (!role.editable)
+      return message.channel.send(`O cargo ${role} está acima do meu. Você pode subir meu cargo?`).then(msg => msg.delete({ timeout: 6000 }))
 
    if (!member.bannable)
       return message.channel.send('Essa pessoa tem mais poder do que eu... Me sinto fraca :cry:').then(msg => msg.delete({ timeout: 5000 }))
@@ -98,18 +123,26 @@ module.exports.run = async (bot, message, args) => {
 
    let reason = args.slice(2).join(" ")
    if (!reason)
-      return message.channel.send('Me fala a razão do mute, rapidinho.').then(msg => msg.delete({ timeout: 3000 }))
+      reason = 'Razão não especificada pelo Moderador.'
 
    let avatar = member.user.displayAvatarURL({ format: 'png' })
 
    const muteembed = new Discord.MessageEmbed()
-      .setTitle("Usuário Mutado")
-      .setAuthor(member.user.tag, avatar)
+      .setAuthor(`Sistema de Mute - ${member.guild.name}`)
       .setColor('#8B0000')
       .addFields(
          {
             name: 'Usuário',
-            value: member.user.tag
+            value: `${member.user}`,
+            inline: `true`
+         },
+         {
+            name: 'Nome do usuário original',
+            value: `${member.user.tag}`
+         },
+         {
+            name: 'ID do usuário',
+            value: member.id
          },
          {
             name: 'Moderador',
@@ -117,12 +150,12 @@ module.exports.run = async (bot, message, args) => {
             inline: true
          },
          {
-            name: 'Tempo',
+            name: 'Tempo do Mute',
             value: time,
             inline: true
          },
          {
-            name: 'Motivo',
+            name: 'Motivo do Mute',
             value: reason
          },
       )
@@ -130,32 +163,26 @@ module.exports.run = async (bot, message, args) => {
       .setTimestamp()
 
    const unmuteembed = new Discord.MessageEmbed()
-      .setTitle("Usuário desmutado")
-      .setAuthor(member.user.tag, avatar)
+      .setAuthor(`Sistema de Mute - ${member.guild.name}`)
       .setColor('#8B0000')
       .addFields(
          {
-            name: 'Usuário',
+            name: 'Usuário destumado com sucesso!',
             value: member.user.tag,
-            inline: true
-         },
-         {
-            name: 'Ficou mutado por',
-            value: time,
             inline: true
          },
       )
       .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
       .setTimestamp()
 
-   const guild = client.guilds.get(guild.id)
-   const role1 = guild.roles.find("name", "Muted")
-   member.roles.add(role1) // Ação Mute
-   member.send(`Você foi mutado no servidor ${member.guild.name}. Veja mais informações do mute no ${logchannel}`).then(msg => message.channel.send(`O Mute foi um sucesso! Estou enviando mais informações no ${logchannel}`)).then(msg => msg.delete({ timeout: 5000 })).then(msg => logchannel.send(muteembed))
+   member.roles.add(role) // Ação Mute
+   member.send(`Você foi mutado no servidor ${member.guild.name}.\n⠀\nVeja mais informações do mute no ${logchannel}`).then(msg => message.channel.send(
+      `O Mute foi um sucesso! Estou enviando mais informações no ${logchannel}`
+   )).then(msg => msg.delete({ timeout: 4000 })).then(msg => logchannel.send(muteembed))
 
    setTimeout(function () {
-      member.roles.remove(role1) //Ação Desmute
-      logchannel.send(unmuteembed).then(msg => member.send(`Você acaba de sair do Mute no ${member.guild.name}`))
+      member.roles.remove(role) //Ação Desmute
+      logchannel.send(unmuteembed).then(msg => member.send(`Seu mute chegou ao fim no servidor ${member.guild.name}`))
    }, ms(time))
 
    module.exports.help = {

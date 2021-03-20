48297f214ac3cef0849c1000b78c4346b1ff5381
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-03-20 16:26:27 -0300

    Update addrole.js
    Fixed and updated

--- moderation/addrole.js
+++ moderation/addrole.js
@@ -1,27 +1,23 @@
 const Discord = require('discord.js')
 
 module.exports.run = async (bot, message, args) => {
-message.delete()
+    message.delete()
 
-let perms = message.member.hasPermission("MANAGE_ROLES")
-if(!perms) return message.channel.send("Você não tem permissão suficiente pra esse comando, que tal chamar um Mod ou Adm?").then(msg => msg.delete({timeout: 3000}))
+    let perms = message.member.hasPermission("MANAGE_ROLES")
+    if (!perms) return message.channel.send("Você não tem permissão suficiente pra esse comando, que tal chamar um Mod ou Adm?").then(msg => msg.delete({ timeout: 3000 }))
 
-let user = message.mentions.members.first()
-if(!user) return message.channel.send("Tenta assim -> `-addrole @user @cargo`").then(msg => msg.delete({timeout: 3000}))
+    let user = message.mentions.members.first()
+    if (!user) return message.channel.send("Tenta assim -> `-addrole @user @cargo`").then(msg => msg.delete({ timeout: 3000 }))
 
-let role = message.mentions.roles.first()
-if(!role) return message.channel.send("Qual é o cargo? Você não me disse.").then(msg => msg.delete({timeout: 3000}))
+    let role = message.mentions.roles.first()
+    if (!role) return message.channel.send("Qual é o cargo? Você não me disse.").then(msg => msg.delete({ timeout: 3000 }))
 
-let avatar = user.user.displayAvatarURL({format: 'png'})
+    if (user.roles.cache.has(role.id)) {
+        return message.channel.send(`${user.user.username} já possui este cargo.`).then(msg => msg.delete({ timeout: 3000 }))
+    }
 
-user.roles.add(role)
-message.channel.send(`${user} recebeu o cargo ${role} com sucesso!`).then(msg => msg.delete({timeout: 6000}))
-}
+    let avatar = user.user.displayAvatarURL({ format: 'png' })
 
-module.exports.help = {
-    name: "addrole",
-    aliases: ['arole'],
-    description: "Add roles to someone",
-    usage: "addrole user role",
-    category: "settings, administration"
+    user.roles.add(role)
+    message.channel.send(`${user.user.username} recebeu o cargo ${role} com sucesso!`).then(msg => msg.delete({ timeout: 6000 }))
 }

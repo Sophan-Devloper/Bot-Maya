e6161a5fc0efbac8351c0e6364d0972c318d4a37
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-03-25 16:16:13 -0300

    Update say.js

--- moderation/say.js
+++ moderation/say.js
@@ -1,11 +1,14 @@
 const Discord = require('discord.js')
 
 module.exports.run = async (client, message, args) => {
-    message.delete()
+  message.delete()
 
-    if (!message.member.permissions.has("ADMINISTRATOR")) 
-    return message.reply("você não pode me usar para mandar mensagens.").then(msg => msg.delete({timeout: 5000}))
-    
+  if (!message.member.permissions.has("ADMINISTRATOR"))
+    return message.reply("você não pode me usar para mandar mensagens.").then(msg => msg.delete({ timeout: 5000 }))
+
+  if (!args[0])
+    return message.channel.send('O que você quer que eu diga?')
+
   const sayMessage = args.join(' ')
-  message.channel.send(sayMessage)
+  message.channel.send(sayMessage + `\n \n- *${message.author.username}*`)
 }

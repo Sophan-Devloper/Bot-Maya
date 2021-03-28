7a76a23a034ae5a8f0811f04d18123d3625f67ef
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-03-28 11:34:26 -0300

    Updated

--- moderation/createchannel.js
+++ moderation/createchannel.js
@@ -1,15 +1,31 @@
 const Discord = require('discord.js')
+const db = require('quick.db')
+
 module.exports.run = (client, message, args) => {
     message.delete()
 
-    if(!message.member.hasPermission('MENAGE_CHANNELS'))
-        return message.channel.send('Você não tem todo esse poder para criar canais, desculpe.').then (msg => msg.delete({timeout: 5000}))
-    
-    if(!args[0])
-        return message.channel.send('Por favor, me diga o nome do canal que você criar\n\n`-createchannel NomeDoCanal`').then(msg => msg.delete({timeout: 9000}))
+    if (!message.member.hasPermission('ADMINISTRATOR')) {
+        const perms = new Discord.MessageEmbed()
+            .setColor('#FF0000')
+            .setTitle('Permissão Necessária: Manusear Canais')
+        return message.channel.send(perms).then(msg => msg.delete({ timeout: 5000 }))
+    }
 
-    message.guild.channels.create(args.slice(0).join(" "),
-    {type: 'text'})
+    if (!args[0]) {
+        let prefix = db.get(`prefix_${message.guild.id}`)
+        if (prefix === null) prefix = "-"
 
-    message.channel.send(`Eu criei o canal que você pediu com sucesso! :hearts:`).then(msg => msg.delete({timeout: 10000}))
+        const noargs = new Discord.MessageEmbed()
+            .setColor('#FF0000')
+            .setTitle('Use o formato correto')
+            .setDescription('`' + prefix + 'createchannel NomeDoCanal`')
+        return message.channel.send(noargs).then(msg => msg.delete({ timeout: 9000 }))
+    }
+
+    message.guild.channels.create(args.slice(0).join(" "), { type: 'text' })
+
+    const sucess = new Discord.MessageEmbed()
+        .setColor('GREEN')
+        .setTitle('Canal criado com sucesso.')
+    message.channel.send(sucess).then(msg => msg.delete({ timeout: 10000 }))
 }

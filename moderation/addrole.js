8f5cc9f399d51b1dd078738a1db7e16c21a72afe
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-03-27 23:38:47 -0300

    Updated

--- moderation/addrole.js
+++ moderation/addrole.js
@@ -4,20 +4,41 @@ module.exports.run = async (bot, message, args) => {
     message.delete()
 
     let perms = message.member.hasPermission("MANAGE_ROLES")
-    if (!perms) return message.channel.send("Você não tem permissão suficiente pra esse comando, que tal chamar um Mod ou Adm?").then(msg => msg.delete({ timeout: 3000 }))
-
-    let user = message.mentions.members.first()
-    if (!user) return message.channel.send("Tenta assim -> `-addrole @user @cargo`").then(msg => msg.delete({ timeout: 3000 }))
-
-    let role = message.mentions.roles.first()
-    if (!role) return message.channel.send("Qual é o cargo? Você não me disse.").then(msg => msg.delete({ timeout: 3000 }))
-
-    if (user.roles.cache.has(role.id)) {
-        return message.channel.send(`${user.user.username} já possui este cargo.`).then(msg => msg.delete({ timeout: 3000 }))
+    if (!perms) {
+        const permss = new Discord.MessageEmbed()
+            .setColor('#FF0000')
+            .setTitle('Permissão Necessária: Manusear Roles (cargos)')
+        return message.channel.send(permss).then(msg => msg.delete({ timeout: 3000 }))
     }
 
-    let avatar = user.user.displayAvatarURL({ format: 'png' })
+    let user = message.mentions.members.first()
+    if (!user) {
+        const userr = new Discord.MessageEmbed()
+            .setColor('#FF0000')
+            .setTitle('Siga o formato')
+            .setDescription('`-addrole @user @cargo`')
+        return message.channel.send(userr).then(msg => msg.delete({ timeout: 3000 }))
+    }
+
+    let role = message.mentions.roles.first()
+    if (!role) {
+        const rolee = new Discord.MessageEmbed()
+            .setColor('#FF0000')
+            .setTitle('Siga o formato')
+            .setDescription('`-addrole @user @cargo`')
+        return message.channel.send(rolee).then(msg => msg.delete({ timeout: 3000 }))
+    }
+
+    if (user.roles.cache.has(role.id)) {
+        const roleee = new Discord.MessageEmbed()
+            .setColor('#FF0000')
+            .setTitle(`${user.user.username} já possui este cargo.`)
+        return message.channel.send(roleee).then(msg => msg.delete({ timeout: 3000 }))
+    }
 
     user.roles.add(role)
-    message.channel.send(`${user.user.username} recebeu o cargo ${role} com sucesso!`).then(msg => msg.delete({ timeout: 6000 }))
+    const sucess = new Discord.MessageEmbed()
+        .setColor('GREEN')
+        .setDescription(`${user.user.username} recebeu o cargo ${role} com sucesso!`)
+    message.channel.send(sucess).then(msg => msg.delete({ timeout: 6000 }))
 }

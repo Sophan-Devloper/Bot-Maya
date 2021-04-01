45ffeb4bc13d1216aa0a8aa24f9f59be984d3482
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-04-01 19:01:28 -0300

    New Feature

--- commands/report.js
+++ commands/report.js
@@ -1,70 +1,52 @@
 const Discord = require('discord.js')
+const db = require('quick.db')
 
-module.exports = {
-    name: "report",
-    category: "moderation",
-    description: "Reports a member",
-    usage: "<mention, id>",
-}
-
-module.exports.run = async (client, message, args) => {
+exports.run = async (client, message, args) => {
     message.delete()
 
-    let channel = message.guild.channels.cache.find(ch => ch.name === "reports")
     let user = message.mentions.members.first()
-    let avatar = message.author.displayAvatarURL({ format: 'png' })
+    let prefix = db.get(`prefix_${message.guild.id}`)
+    if (prefix === null) prefix = "-"
 
-    if (!channel)
-        return message.channel.send("Por favor, peça para que um Adm crie um Canal com o nome `reports` \nCopie e cole pra eu criar um pra você \n`-createchannel reports`").then(msg => msg.delete({ timeout: 10000 })).catch(err => { return })
-
-    if (message.content === '-report')
-        return message.channel.send("Hey, tenta fazer assim -> `-report Seu Reporte.` \nQuer reportar um membro? -> `-report @user Motivo do seu Reporte.`").then(msg => msg.delete({ timeout: 10000 })).catch(err => { return })
-
-    if (!user) {
-        const embed = new Discord.MessageEmbed()
-            .setColor("#DCDCDC")
-            .setAuthor('Reporte Recebido')
-            .addFields(
-                {
-                    name: 'Autor do Reporte',
-                    value: message.author,
-                    inline: true
-                },
-                {
-                    name: 'Membro Reportado',
-                    value: 'Nenhum membro foi reportado',
-                    inline: true
-                },
-                {
-                    name: 'Razão do Reporte',
-                    value: args.slice(0).join(" ")
-                }
-            )
-            .setTimestamp()
-        return channel.send(embed).then(msg => message.author.send(`O seu reporte foi enviado para a moderação do *${message.guild.name}* com sucesso.`))
-    } else {
-
-        const embed1 = new Discord.MessageEmbed()
-            .setColor("#DCDCDC")
-            .setAuthor('Reporte Recebido')
-            .addFields(
-                {
-                    name: 'Autor do Reporte',
-                    value: message.author,
-                    inline: true
-                },
-                {
-                    name: 'Membro Reportado',
-                    value: user,
-                    inline: true
-                },
-                {
-                    name: 'Razão do Reporte',
-                    value: args.slice(1).join(" ")
-                }
-            )
-            .setThumbnail(user.user.displayAvatarURL({ dynamic: true }))
-            .setTimestamp()
-        return channel.send(embed1).then(msg => message.author.send(`O seu reporte foi enviado para a moderação do *${message.guild.name}* com sucesso.`))
+    var channel = db.get(`reportchannel_${message.guild.id}`)
+    if (channel === null) {
+        const nochannel = new Discord.MessageEmbed()
+            .setColor('#FF0000')
+            .setTitle('Nenhum canal de report definido.')
+            .setDescription('`' + prefix + 'setreportchannel #canal`')
+        return message.channel.send(nochannel).then(msg => msg.delete({ timeout: 15000 })).catch(err => { return })
     }
+
+    if (!db.get(`reportchannel_${message.guild.id}`)) {
+        const nochanel = new Discord.MessageEmbed()
+            .setColor('#FF0000')
+            .setTitle('Parece que o canal de report foi excluido.')
+            .setDescription('`' + prefix + 'setreportchannel #canal`')
+        return message.channel.send(nochanel).then(msg => msg.delete({ timeout: 15000 })).catch(err => { return })
+    }
+
+    if (!user) user = 'Nenhum membro foi reportado'
+
+    const embed1 = new Discord.MessageEmbed()
+        .setColor("BLUE")
+        .setTitle('Novo Reporte Recebido')
+        .addFields(
+            {
+                name: 'Autor do Reporte',
+                value: message.author,
+                inline: true
+            },
+            {
+                name: 'Membro Reportado',
+                value: user,
+                inline: true
+            },
+            {
+                name: 'Razão do Reporte',
+                value: args.slice(1).join(" ")
+            }
+        )
+        .setThumbnail(user.user.displayAvatarURL({ dynamic: true }))
+        .setTimestamp()
+    client.channels.cache.get(channel).send(joinembed).send(embed1).then(msg => message.author.send(`O seu reporte foi enviado para a moderação do *${message.guild.name}* com sucesso.`))
 }
---
+++ moderation/setreportchannel.js
@@ -0,0 +1,69 @@
+const Discord = require('discord.js')
+const db = require('quick.db')
+
+module.exports = {
+    run: async (client, message, args) => {
+        message.delete()
+
+        let permss = message.member.hasPermission("ADMINISTRATOR")
+        if (!permss) {
+            const noperm = new Discord.MessageEmbed()
+                .setColor('#FF0000')
+                .setTitle('Permissão Necessária: Administrador')
+
+            return message.channel.send(noperm).then(msg => msg.delete({ timeout: 7000 })).catch(err => { return })
+        }
+
+        if (!args[0]) {
+            let prefix = db.get(`prefix_${message.guild.id}`)
+            if (prefix === null) prefix = "-"
+
+            const noargs = new Discord.MessageEmbed()
+                .setColor('#FF0000') // red
+                .setTitle('`' + prefix + 'setreportchannel #Canal`')
+            return message.channel.send(noargs).then(msg => msg.delete({ timeout: 5000 })).catch(err => { return })
+        }
+
+        if (args[0] === 'off') {
+            let prefix = db.get(`prefix_${message.guild.id}`)
+            if (prefix === null) prefix = "-"
+
+            const semcanal = new Discord.MessageEmbed()
+                .setColor('#ff0000')
+                .setTitle('O Report Channel não pode ser desativado. (Por enquanto)')
+                .setDescription('Caso queira trocar de canal, use o comando \n`' + prefix + 'setreportchannel #Canal`')
+
+            return message.channel.send(semcanal).then(msg => msg.delete({ timeout: 5000 })).catch(err => { return })
+        }
+
+        var channel = message.mentions.channels.first()
+        if (!channel) {
+            let prefix = db.get(`prefix_${message.guild.id}`)
+            if (prefix === null) prefix = "-"
+            const nochannel = new Discord.MessageEmbed()
+                .setColor('#FF0000') // red
+                .setTitle('' + prefix + 'setreportchannel #Canal')
+
+            return message.channel.send(nochannel).then(msg => msg.delete({ timeout: 10000 })).catch(err => { return })
+        }
+
+        var atual = db.get(`reportchannel_${message.guild.id}`)
+        if (channel.id === atual) {
+
+            const iqual = new Discord.MessageEmbed()
+                .setColor('#FF0000') // Red
+                .setTitle('Este canal já foi definido como Report Channel!')
+
+            return message.channel.send(iqual)
+        } else if (args[0] !== atual) {
+            db.set(`reportchannel_${message.guild.id}`, channel.id)
+
+            const sucess = new Discord.MessageEmbed()
+                .setColor('GREEN')
+                .setTitle('Report Channel Definido!')
+                .setDescription(`Canal escolhido: ${channel}`)
+
+            return message.channel.send(sucess)
+        }
+    }
+}

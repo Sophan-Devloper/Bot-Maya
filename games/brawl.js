fdbe3ef4524776e5e08438d08dcb7cbc03703aee
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-04-04 19:37:28 -0300

    Update brawl.js

--- games/brawl.js
+++ games/brawl.js
@@ -1,5 +1,68 @@
+const Discord = require('discord.js')
+
 exports.run = async (client, message, args) => {
   message.delete()
 
-  return message.channel.send('Tem 2 jogos no meu banco de dados com o apelido "Brawl", qual você quer?\n`-BrawlStars`\n`-Brawlhalla`').then(msg => msg.delete({ timeout: 8000 })).catch(err => { return })
+  const games = new Discord.MessageEmbed()
+    .setColor('BLUE')
+    .setTitle('Existe 2 jogos com o apelido Brawl no meu banco de dados')
+    .setDescription('☝️ Brawlhalla\n✌️ Brawlstars')
+
+  await message.channel.send(games).then(msg => {
+    msg.react('☝️') // Check
+    msg.react('✌️') // X
+
+    msg.awaitReactions((reaction, user) => {
+      if (message.author.id !== user.id) return
+
+      if (reaction.emoji.name === '☝️') { // Sim
+        msg.delete()
+
+        var game = 'Brawlhalla'
+        var link1 = 'https://store.steampowered.com/app/291550/Brawlhalla/'
+        var link2 = 'https://play.google.com/store/apps/details?id=air.com.ubisoft.brawl.halla.platform.fighting.action.pvp&hl=pt_BR'
+        var st = 'Steam'
+        var ps = 'Play Store'
+        var pt = 'PlayStation'
+        var w = 'Microsoft Windows'
+        var nsw = 'Nintendo Switch'
+        var xbo = 'Xbox One'
+        var mc = 'MacOS'
+        var ios = 'iOS'
+        var an = 'Android'
+
+        const GameEmbed = new Discord.MessageEmbed()
+          .setColor('BLUE')
+          .setTitle(`${game}`)
+          .addField(`${st}`, `${link1}`)
+          .addField(`${ps}`, `${link2}`)
+          .setFooter(`Plataformas: ${pt} 4, ${nsw}, ${an}, ${xbo}, ${mc}, ${w}, ${ios}`)
+        return message.channel.send(GameEmbed).then(msg => msg.delete({ timeout: 10000 })).catch(err => { return })
+      }
+      if (reaction.emoji.name === '✌️') { // Não
+        msg.delete()
+
+        var game = 'Brawlhalla'
+        var link1 = 'https://store.steampowered.com/app/291550/Brawlhalla/'
+        var link2 = 'https://play.google.com/store/apps/details?id=air.com.ubisoft.brawl.halla.platform.fighting.action.pvp&hl=pt_BR'
+        var st = 'Steam'
+        var ps = 'Play Store'
+        var pt = 'PlayStation'
+        var w = 'Microsoft Windows'
+        var nsw = 'Nintendo Switch'
+        var xbo = 'Xbox One'
+        var mc = 'MacOS'
+        var ios = 'iOS'
+        var an = 'Android'
+
+        const GameEmbed = new Discord.MessageEmbed()
+          .setColor('BLUE')
+          .setTitle(`${game}`)
+          .addField(`${st}`, `${link1}`)
+          .addField(`${ps}`, `${link2}`)
+          .setFooter(`Plataformas: ${pt} 4, ${nsw}, ${an}, ${xbo}, ${mc}, ${w}, ${ios}`)
+        return message.channel.send(GameEmbed).then(msg => msg.delete({ timeout: 10000 })).catch(err => { return })
+      }
+    })
+  })
 }

46466e267218c55940ec7a65727c950819d72e65
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-04-05 15:48:23 -0300

    Updated

---
+++ commands/cal.js
@@ -0,0 +1,30 @@
+const math = require('mathjs')
+const Discord = require('discord.js')
+
+exports.run = async (client, message, args) => {
+
+    if (!args[0]) {
+        const noargs = new Discord.MessageEmbed()
+            .setColor('#FF0000')
+            .setTitle('Formato suportado')
+            .setDescription('`Adição: 10 + 10`\n`Divisão: 10 / 10`\n`Subtração: 10 - 10`\n`Multiplicação: 10 * 10`')
+        return message.channel.send(noargs).then(msg => msg.delete({ timeout: 15000 })).catch(err => { return })
+    }
+
+    let resp
+    try {
+        resp = math.evaluate(args.join(" "))
+    } catch (e) {
+        const noargs = new Discord.MessageEmbed()
+            .setColor('#FF0000')
+            .setTitle('Por favor, diga uma conta válida')
+            .setDescription('Exemplo: 10 + 10\nExemplo: 10 / 10\nExemplo: 10 - 10\nExemplo: 10 * 10')
+        return message.channel.send(noargs)
+    }
+
+    const embed = new Discord.MessageEmbed()
+        .setColor('BLUE')
+        .addField('Conta', `\`\`\`css\n${args.join(' ')}\`\`\``)
+        .addField('Resultado', `\`\`\`css\n${resp}\`\`\``)
+    return message.channel.send(embed)
+}
--- commands/covid.js
+++ commands/covid.js
@@ -1,58 +1,60 @@
-const axios = require('axios');
-const { MessageEmbed } = require('discord.js')
+const axios = require('axios')
+const Discord = require('discord.js')
 
-module.exports = {
-    name: "corona",
-    category: "extra",
-    run: async (client, message, args) => {
-        message.delete()
-        const baseUrl = "https://corona.lmao.ninja/v2";
+exports.run = async (client, message, args) => {
+    message.delete()
 
-        let url, response, corona;
+    const baseUrl = "https://corona.lmao.ninja/v2"
+    let url, response, corona;
 
-        try {
-            url = args[0] ? `${baseUrl}/countries/${args[0]}` : `${baseUrl}/all`
-            response = await axios.get(url)
-            corona = response.data
-        } catch (error) {
-            return message.channel.send(`***${args[0]}*** não existe ou os dados não foram publicados pela OMS (Organização Mundial da Saúde`)
-        }
-
-        const embed = new MessageEmbed()
-            .setTitle(args[0] ? `${args[0].toUpperCase()} Status` : 'Dados Mundiais da COVID-19')
-            .setColor('#fb644c')
-            .setThumbnail(args[0] ? corona.countryInfo.flag : 'https://i.giphy.com/YPbrUhP9Ryhgi2psz3.gif')
-            .addFields(
-                {
-                    name: '⛑️ Casos',
-                    value: corona.cases.toLocaleString()
-                },
-                {
-                    name: '😥 Mortes',
-                    value: corona.deaths.toLocaleString()
-                },
-                {
-                    name: '🥳 Recuperados',
-                    value: corona.recovered.toLocaleString()
-                },
-                {
-                    name: '✅ Ativos',
-                    value: corona.active.toLocaleString()
-                },
-                {
-                    name: '🚨 Casos Críticos',
-                    value: corona.critical.toLocaleString()
-                },
-                {
-                    name: ':heart: Recuperados Hoje',
-                    value: corona.todayRecovered.toLocaleString().replace("-", "")
-                },
-                {
-                    name: ':broken_heart: Mortes Hoje',
-                    value: corona.todayDeaths.toLocaleString()
-                })
-            .setFooter(message.author.username, message.author.displayAvatarURL())
-
-        await message.channel.send(embed).then(msg => message.delete({ timeout: 20000 })).catch(err => {return})
+    try {
+        url = args[0] ? `${baseUrl}/countries/${args[0]}` : `${baseUrl}/all`
+        response = await axios.get(url)
+        corona = response.data
+    } catch (error) {
+        const loading = new Discord.MessageEmbed()
+            .setColor('BLUE')
+            .setTitle('🔄 Loading...')
+        const noerl = new Discord.MessageEmbed()
+            .setColor('#FF0000')
+            .setTitle(`O argumento ***${args[0]}*** não existe ou os dados não foram publicados pela OMS (Organização Mundial da Saúde`)
+        return message.channel.send(loading).then(msg => msg.delete({ timeout: 7000 })).then(msg => msg.channel.send(noerl))
     }
+
+    const embed = new Discord.MessageEmbed()
+        .setTitle(args[0] ? `${args[0].toUpperCase()} Status` : 'Dados Mundiais da COVID-19')
+        .setColor('BLUE')
+        .setThumbnail(args[0] ? corona.countryInfo.flag : 'https://i.giphy.com/YPbrUhP9Ryhgi2psz3.gif')
+        .addFields(
+            {
+                name: '⛑️ Casos',
+                value: corona.cases.toLocaleString()
+            },
+            {
+                name: '😥 Mortes',
+                value: corona.deaths.toLocaleString()
+            },
+            {
+                name: '🥳 Recuperados',
+                value: corona.recovered.toLocaleString()
+            },
+            {
+                name: '✅ Ativos',
+                value: corona.active.toLocaleString()
+            },
+            {
+                name: '🚨 Casos Críticos',
+                value: corona.critical.toLocaleString()
+            },
+            {
+                name: ':heart: Recuperados Hoje',
+                value: corona.todayRecovered.toLocaleString().replace("-", "")
+            },
+            {
+                name: ':broken_heart: Mortes Hoje',
+                value: corona.todayDeaths.toLocaleString()
+            })
+        .setFooter(message.author.username, message.author.displayAvatarURL())
+
+    await message.channel.send(embed).then(msg => message.delete({ timeout: 20000 })).catch(err => { return })
 }

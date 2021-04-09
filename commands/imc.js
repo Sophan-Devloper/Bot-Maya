651ee6d9e97b9e74036b7224b7f7f0f3329a45fe
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-04-09 16:15:47 -0300

    Update imc.js

--- commands/imc.js
+++ commands/imc.js
@@ -1,42 +1,92 @@
-const { MessageEmbed } = require("discord.js");
+const Discord = require("discord.js")
+const db = require('quick.db')
 
-module.exports = {
-    name: 'imc',
-    description: "Calcule seu imc",
-    usage: "imc <peso> <altura>",
-    aliases: [],
-    run: async (client, message, args) => {
+exports.run = async (client, message, args) => {
 
-        const weight = args[0]
-        const height = args[1]
+    const weight = args[0]
+    const height = args[1]
 
-        if (!args[0])
-            return message.channel.send('Indice de Massa Corporal\n Por favor, me diga seu peso e altura para que eu possa te dizer o seu IMC\n \nExemplo: `imc Peso Altura`').then(msg => msg.delete({ timeout: 7000 })).catch(err => { return })
-        if (!args[1]) {
-            const imcError = new MessageEmbed()
-                .setDescription('Você tem que colocar seu peso e altura. Siga este formato.\
-\n\n(Format: imc Peso Altura)\n\nObs: Peso é em Kilogramas, exemplo: 75.5, 80\n\nAltura é em centimentros, exemplo: 170, 175')
-                .setColor("BLUE")
+    if (!args[1]) {
+        let prefix = db.get(`prefix_${message.guild.id}`)
+        if (prefix === null) { prefix = "-" }
 
-            return message.channel.send(imcError).then(msg => msg.delete({ timeout: 7000})).catch(err => { return })
-        }
+        const imcError = new Discord.MessageEmbed()
+            .setColor("#FF0000")
+            .setTitle('Siga o formato correto')
+            .setDescription('`' + prefix + 'imc Peso Altura`' + '\n \n**Atenção**\nPeso em **Kilogramas** e Altura em **centimentros**\n \n')
+            .addFields(
+                {
+                    name: 'Exemplo',
+                    value: `${prefix}imc 70 65`
+                },
+                {
+                    name: 'Exemplo 2',
+                    value: `${prefix}imc 78.8 185`
+                }
+            )
 
-        const imc = (weight / ((height * height) / 10000)).toFixed(2);
-
-        let category;
-        if (imc < 18.5) category = "Abaixo do peso"
-        if (imc > 24.9) category = "Acima do peso"
-        if (imc > 30) category = "Obesidade"
-        if (imc < 24.9 && imc > 18.5) category = "Padrão"
-
-        const embed = new MessageEmbed()
-            .setTitle(`${message.author.username}\'s IMC`)
-            .addField('Peso', weight)
-            .addField('Altura', height)
-            .addField('IMC', imc)
-            .addField('Categoria', category)
-            .setColor("#DCDCDC")
-
-        message.channel.send(embed)
+        return message.channel.send(imcError)
     }
+
+    if (isNaN(args[0])) {
+        let prefix = db.get(`prefix_${message.guild.id}`)
+        if (prefix === null) { prefix = "-" }
+
+        const imcError = new Discord.MessageEmbed()
+            .setColor("#FF0000")
+            .setTitle('Siga o formato correto')
+            .setDescription('`' + prefix + 'imc Peso Altura)`' + '\n \nAtenção: Peso em **Kilogramas** e **Altura** em centimentros')
+            .addFields(
+                {
+                    name: 'Exemplo',
+                    value: `${prefix}imc 70 65`
+                },
+                {
+                    name: 'Exemplo 2',
+                    value: `${prefix}imc 78.8 185`
+                }
+            )
+
+        return message.channel.send(imcError)
+    }
+
+    if (isNaN(args[1])) {
+        let prefix = db.get(`prefix_${message.guild.id}`)
+        if (prefix === null) { prefix = "-" }
+
+        const imcError = new Discord.MessageEmbed()
+            .setColor("#FF0000")
+            .setTitle('Siga o formato correto')
+            .setDescription('`' + prefix + 'imc Peso Altura)`' + '\n \nAtenção: Peso em **Kilogramas** e **Altura** em centimentros')
+            .addFields(
+                {
+                    name: 'Exemplo',
+                    value: `${prefix}imc 70 65`
+                },
+                {
+                    name: 'Exemplo 2',
+                    value: `${prefix}imc 78.8 185`
+                }
+            )
+
+        return message.channel.send(imcError)
+    }
+
+    const imc = (weight / ((height * height) / 10000)).toFixed(2)
+
+    let category;
+    if (imc < 18.5) category = "Abaixo do peso"
+    if (imc > 24.9) category = "Acima do peso"
+    if (imc > 30) category = "Obesidade"
+    if (imc < 24.9 && imc > 18.5) category = "Padrão"
+
+    const embed = new Discord.MessageEmbed()
+        .setTitle(`${message.author.username}\'s IMC`)
+        .addField('Peso', weight)
+        .addField('Altura', height)
+        .addField('IMC', imc)
+        .addField('Categoria', category)
+        .setColor("#DCDCDC")
+
+    await message.channel.send(embed)
 }

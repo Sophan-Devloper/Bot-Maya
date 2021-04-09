740bd6ec22af9e5963d720c69b607336017fb781
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-04-09 13:43:44 -0300

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,24 +1,21 @@
 const Discord = require('discord.js')
 const db = require('quick.db')
 
-module.exports = {
-    run: async (client, message, args) => {
+exports.run = async (client, message, args) => {
 
-        let prefix = db.get(`prefix_${message.guild.id}`)
-        if (prefix === null) prefix = "-"
+    let prefix = db.get(`prefix_${message.guild.id}`)
+    if (prefix === null) prefix = "-"
 
-        const prefixembed = new Discord.MessageEmbed()
-            .setColor('#DCDCDC')
-            .setTitle('ℹ️ Informações sobre Prefixo')
-            .setDescription('Prefixo é o simbolo que você utiliza para executar um comando em um bot no Discord.\nExemplo: `' + prefix + 'prefix` ou `' + prefix + 'help`')
-            .addFields(
-                {
-                    name: '💡 Meus comandos de Prefix',
-                    value: '`' + prefix + 'setprefix` Escolha meu prefixo\n`' + prefix + 'resetprefix` Resete meu prefixo para `-`'
-                }
-            )
-            .setFooter(message.author.tag, message.author.displayAvatarURL())
+    const prefixembed = new Discord.MessageEmbed()
+        .setColor('#DCDCDC')
+        .setTitle('ℹ️ Informações sobre Prefixo')
+        .setDescription('Prefixo é o simbolo que você utiliza para executar um comando em um bot no Discord.\nExemplo: `' + prefix + 'prefix` ou `' + prefix + 'help`')
+        .addFields(
+            {
+                name: '💡 Meus comandos de Prefix',
+                value: '`' + prefix + 'setprefix` Escolha meu prefixo\n`' + prefix + 'resetprefix` Resete meu prefixo para `-`'
+            }
+        )
 
-        await message.channel.send(prefixembed)
-    }
+    return message.channel.send(`${message.author}`, prefixembed)
 }

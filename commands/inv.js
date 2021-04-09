5cd750452ec6e784a93399df19167339e5caf5a0
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-04-09 13:58:20 -0300

    Updated

--- commands/inv.js
+++ commands/inv.js
@@ -1,21 +1,9 @@
 const Discord = require('discord.js')
 
-module.exports.run = async (bot, message, args) => {
+exports.run = async (bot, message, args) => {
     
     var embed = new Discord.MessageEmbed()
         .setColor('#D8901B')
-        .setTitle(`Link para me adicionar no seu servidor`)
-        .addFields(
-            {
-                name: 'Link direto',
-                value: '[Clique aqui](https://discord.com/api/oauth2/authorize?client_id=821471191578574888&permissions=8&scope=bot)',
-                inline: true
-            },
-            {
-                name: 'QR Code',
-                value: '`-qrinvite`',
-                inline: true
-            }
-        )
-    await message.channel.send(embed)
+        .setDescription('❤️ [Clique aqui para me adicionar](https://discord.com/api/oauth2/authorize?client_id=821471191578574888&permissions=8&scope=bot)')
+    return message.channel.send(`${message.author}, obrigada por querer me adicionar no seu servidor.`, embed)
 }
--- commands/invites.js
+++ commands/invites.js
@@ -1,39 +1,35 @@
 const { MessageEmbed } = require("discord.js")
 
-module.exports = {
-    name: 'invites',
-    description: "Gives You the list of top 5 people with most invites to your server!",
-    usage: "?invites",
-    aliases: ['inv'],
-    run: async(client, message, args) => {
-        message.guild.fetchInvites().then((invites) => {
-            const inviteCounter = {}
+exports.run = async (client, message, args) => {
+
+    message.guild.fetchInvites().then((invites) => {
+        const inviteCounter = {}
+
+        invites.forEach((invite => {
+            const { uses, inviter } = invite
+            const { username, discriminator } = inviter
+
+            const name = `${inviter}`
+
+            inviteCounter[name] = (inviteCounter[name] || 0) + uses
+        }))
+
+        let replyText = new MessageEmbed()
+            .setTitle(`📩 Convites ${message.guild.name}`)
+            .setDescription(` \n`)
+            .setColor("BLUE")
+            .setFooter('Apareceu o ID? O membro saiu do servidor')
+
+        const sortedInvites = Object.keys(inviteCounter).sort((a, b) => inviteCounter[b] - inviteCounter[a])
+
+        if (sortedInvites.length > 5) sortedInvites.length = 5
+        else if (sortedInvites.length > 5) sortedInvites.length = sortedInvites.length
 
 
-            invites.forEach((invite => {
-                const { uses, inviter } = invite
-                const { username, discriminator } = inviter
-
-                const name = `${inviter}`
-
-                inviteCounter[name] = (inviteCounter[name] || 0) + uses
-             }))
-
-             let replyText = new MessageEmbed()
-             .setDescription('Convites: \n')
-             .setColor("BLUE")
-
-             const sortedInvites = Object.keys(inviteCounter).sort((a, b) => inviteCounter[b] - inviteCounter[a])
-
-             if (sortedInvites.length > 5) sortedInvites.length = 5
-             else if(sortedInvites.length > 5) sortedInvites.length = sortedInvites.length
-
-
-             for(const invite of sortedInvites) {
-                 const count = inviteCounter[invite]
-                 replyText.description += `\n${invite} convidou ${count} membro(s).`
-             }
-             message.channel.send(replyText)
-        })
-    }
+        for (const invite of sortedInvites) {
+            const count = inviteCounter[invite]
+            replyText.description += `\n${invite} convidou ${count} membro(s).`
+        }
+        message.channel.send(replyText)
+    })
 }

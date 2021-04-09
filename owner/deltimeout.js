3b786430ac1936e1bb97ba9aeb0a3c851a1c6488
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-04-09 18:43:52 -0300

    Update deltimeout.js

--- owner/deltimeout.js
+++ owner/deltimeout.js
@@ -2,22 +2,22 @@ const Discord = require('discord.js')
 const db = require('quick.db')
 
 module.exports.run = async (bot, message, args) => {
-     
 
     let user = message.mentions.users.first() || message.author
 
     const rody = message.author.id === ("451619591320371213")
-    if (!rody)
+    if (!rody) {
         return message.channel.send('⚠️ Este comando é restrito.').then(msg => msg.delete({ timeout: 5000 }))
+    }
 
     db.delete(`dailyxp_${user.id}`)
     db.delete(`rptimeout_${user.id}`)
     db.delete(`robtime_${user.id}`)
     db.delete(`lotery_${user.id}`)
     db.delete(`work_${user.id}`)
-    db.delete(`slut_${message.author.id}`)
-    db.delete(`preso_${message.author.id}`)
-    db.delete(`pego_${message.author.id}`)
-    db.delete(`procurado_${message.author.id}`)
-    message.channel.send(`Timeout resetado.`).then(msg => msg.delete({ timeout: 6000 }))
+    db.delete(`slut_${user.id}`)
+    db.delete(`preso_${user.id}`)
+    db.delete(`pego_${user.id}`)
+    db.delete(`procurado_${user.id}`)
+    return message.channel.send(`Timeout resetado.`).then(msg => msg.delete({ timeout: 6000 }))
 }

066b2a5710a537862c25f7ac9156d7e1161bb63a
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-04-09 13:08:12 -0300

    Update clima.js

--- commands/clima.js
+++ commands/clima.js
@@ -1,28 +1,43 @@
 const Discord = require("discord.js")
 const weather = require("weather-js")
+const db = require('quick.db')
 
 exports.run = async (client, message, args) => {
 
   let city = args.join(" ")
-  let degreetype = "C"; // Mude para Fahrenheit F
+  let degreetype = "C" // Mude para Fahrenheit F
 
   await weather.find({ search: city, degreeType: degreetype }, function (err, result) {
 
-    if (!city)
-      return message.channel.send("Ei, espera. Qual é a cidade?\nTenta assim `-clima NomeDaCidade` etc").then(msg => msg.delete({ timeout: 6000 })).catch(err => { return })
+    if (!city) {
+      let prefix = db.get(`prefix_${message.guild.id}`)
+      if (prefix === null) { prefix = "-" }
 
-    if (err || result === undefined || result.length === 0)
-      return message.channel.send("Eu não conheço essa cidade, acho que você escreveu errado.").then(msg => msg.delete({ timeout: 6000 })).catch(err => { return })
+      const nocity = new Discord.MessageEmbed()
+        .setColor('#FF0000')
+        .setTitle('Siga o formato correto')
+        .setDescription('`' + prefix + 'clima SP/RJ/MG ou o nome da Cidade/Estado`')
+      return message.channel.send(nocity)
+    }
+
+    if (err || result === undefined || result.length === 0) {
+      const noresult = new Discord.MessageEmbed()
+        .setColor('#FF0000')
+        .setTitle('Parece que ocorreu um erro no meu sistema de busca')
+        .setDescription('`Nenhuma cidade/estado foi encontrado`')
+      return message.channel.send(noresult)
+    }
 
     let current = result[0].current
     let location = result[0].location
 
     const embed = new Discord.MessageEmbed()
+      .setColor('BLUE')
       .setAuthor(current.observationpoint)
       .setDescription(`> ${current.skytext}`)
       .setThumbnail(current.imageUrl)
       .setTimestamp()
-      .setColor(0x7289DA)
+
     embed.addField("Latitude", location.lat, true)
       .addField("Longitude", location.long, true)
       .addField("Temperatura Térmica", `${current.feelslike}° Graus`, true)
@@ -34,18 +49,6 @@ exports.run = async (client, message, args) => {
       .addField("Observação TimeTemp", current.observationtime, true)
       .setFooter(message.author.tag, message.author.displayAvatarURL())
 
-    return message.channel.send(embed).then(msg => msg.delete({ timeout: 10000 })).catch(err => { return })
+    return message.channel.send(embed)
   })
-}
-
-exports.help = {
-  name: "clima",
-  description: "Responde ao clima correspondente a área de requisição.",
-  usage: "clima <cidade/cep>",
-  example: "clima São Paulo"
-}
-
-exports.conf = {
-  aliases: [],
-  cooldown: 7.5
 }

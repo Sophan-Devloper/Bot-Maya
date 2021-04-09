b9d3c2cedaecf8ee90084d730d983411eef8aff8
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-04-09 14:05:32 -0300

    Update user.js

--- commands/user.js
+++ commands/user.js
@@ -1,9 +1,8 @@
 const Discord = require("discord.js")
 
 exports.run = async (client, message, args) => {
- 
 
-  let user = message.mentions.users.first () || client.users.cache.get(args[0]) || message.author
-  
-await message.channel.send(`${user.tag}`).then(msg => msg.delete({timeout: 8000}))
+  let user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author
+
+  return message.channel.send(`${user.tag}`)
 }

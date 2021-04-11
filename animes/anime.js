17408c73ad181cce3e7888652e60c4aacfe4db02
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-04-11 19:40:59 -0300

    Update anime.js

--- animes/anime.js
+++ animes/anime.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js")
 
 exports.run = async (client, message, args) => {
-    return message.channel.send('Comando em reforma')
+    message.delete()
+    return message.channel.send('Comando em reforma').then(msg => msg.delete({ timeout: 5000 })).catch(err => { return })
 }

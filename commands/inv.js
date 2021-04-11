11fda9f89c8376fcca6d780b5d5c8c3c362e168d
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-04-11 00:57:21 -0300

    updated

--- commands/inv.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js')
-
-exports.run = async (bot, message, args) => {
-    
-    var embed = new Discord.MessageEmbed()
-        .setColor('#D8901B')
-        .setDescription('❤️ [Clique aqui para me adicionar](https://discord.com/api/oauth2/authorize?client_id=821471191578574888&permissions=8&scope=bot)')
-    return message.channel.send(`${message.author}, obrigada por querer me adicionar no seu servidor.`, embed)
-}
--- commands/ping.js
+++
@@ -1,3 +0,0 @@
-module.exports.run = async (client, message, args) => {
-  await message.channel.send(`⏳ ${Math.round(client.ws.ping)}ms`)
-}

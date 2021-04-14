8a34b6a880442aa5d55c9c98af285da45b79419d
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-04-14 14:36:52 -0300

    Update reboot.js

--- owner/reboot.js
+++ owner/reboot.js
@@ -5,5 +5,5 @@ exports.run = async (client, message, args) => {
         message.delete()
         return message.channel.send('⚠️ Este comando é um comando restrito.')
     }
-    return message.channel.send('Rebooting...').then(msg => msg.delete({ timeout: 6000 })).then(m => m.channel.send('Erros encontrado: 0')).then(m => m.delete({ timeout: 5000 }))
+    return message.channel.send('Rebooting...').then(msg => msg.delete({ timeout: 15000 }))
 }

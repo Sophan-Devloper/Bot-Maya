25f2df2af03fafc14ba5fc3fba9c5532bf0399af
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-04-14 14:23:45 -0300

    Updated

--- owner/leaveall.js
+++
@@ -1,6 +0,0 @@
-exports.run = async (client, message, args) => {
-    const rody = message.author.id === ("451619591320371213")
-    if (!rody)
-        message.delete()
-    return message.channel.send('⚠️ Este comando é um comando restrito.').then(msg => msg.delete({ timeout: 5000 }))
-}
--- owner/turnoff.js
+++
@@ -1,6 +0,0 @@
-exports.run = async (client, message, args) => {
-        const rody = message.author.id === ("451619591320371213")
-        if (!rody) {
-                return message.channel.send('⚠️ Este comando é um comando restrito.')
-        }
-}

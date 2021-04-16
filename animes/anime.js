4a981fce08ce5fc9f18b9a847556b7e0af44577e
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-04-16 15:33:35 -0300

    Update anime.js

--- animes/anime.js
+++ animes/anime.js
@@ -1,6 +1,14 @@
 const Discord = require("discord.js")
 
 exports.run = async (client, message, args) => {
+
+    if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) {
+      const adm = new Discord.MessageEmbed()
+        .setColor('#FF0000')
+        .setTitle('Eu preciso da permissão "Gerenciar Mensagens" para utilizar esta função.')
+      return message.inlineReply(adm)
+    }
+    
     message.delete()
     return message.inlineReply('Comando em reforma').then(msg => msg.delete({ timeout: 5000 })).catch(err => { return })
 }

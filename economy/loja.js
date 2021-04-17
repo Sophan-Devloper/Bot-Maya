b58c60b6f7480ca8b4b9768e6db2f47e5b87c3c4
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-04-17 13:01:07 -0300

    updated

--- economy/loja.js
+++ economy/loja.js
@@ -8,6 +8,7 @@ exports.run = async (client, message, args) => {
 
     var star = '<:estrelinha:831161441847345202>'
     var loli = '<:Loli:831571527744356422>'
+    var StarM  = '<:starM:832974891635572787>'
 
     const loja = new Discord.MessageEmbed()
         .setColor('YELLOW')
@@ -15,18 +16,22 @@ exports.run = async (client, message, args) => {
         .setDescription('Aqui na Lojinha Maya, você pode comprar várias coisas para ter acesso a comandos e funções incriveis.')
         .addFields(
             {
-                name: 'Itens Disponiveis',
-                value: '🔰 `Título` 10.000.000<:estrelinha:831161441847345202>MPoints\n🎣 `Vara de Pesca` 5.000 <:estrelinha:831161441847345202>MPoints\n🔫 `Arma` 100.000 <:estrelinha:831161441847345202>MPoints\n⛏️ `Picareta` 350 <:estrelinha:831161441847345202>MPoints\n🪓 `Machado` 400 <:estrelinha:831161441847345202>MPoints\n🪱 `Isca` 10 <:estrelinha:831161441847345202>\n🥤 `Água` 10 <:estrelinha:831161441847345202>MPoints'
+                name: 'Disponiveis',
+                value: '🛡️ `Escudo` (Em Breve)\n🎣 `Vara de Pesca` 5.000 <:estrelinha:831161441847345202>MPoints\n🔫 `Arma` 100.000 <:estrelinha:831161441847345202>MPoints\n⛏️ `Picareta` 350 <:estrelinha:831161441847345202>MPoints\n🪓 `Machado` 400 <:estrelinha:831161441847345202>MPoints\n🪱 `Isca` 10 <:estrelinha:831161441847345202>MPoints\n🥤 `Água` 10 <:estrelinha:831161441847345202>MPoints'
             },
             {
-                name: 'Itens Especiais',
-                value: '<:Loli:831571527744356422> Loli\n🔪 Faca\n<:fossil:831859111578173450> Fossil\n🦣 Mamute\n🛡️ Em breve'
+                name: 'Jogos',
+                value: '<:Loli:831571527744356422> `Loli` (Pesca)\n🔪 `Faca` (Pesca)\n<:fossil:831859111578173450> `Fossil` (Mineração)\n🦣 `Mamute` (Mineração)'
+            },
+            {
+                name: 'Perfil',
+                value: '<:starM:832974891635572787> `Estrela` (Em breve)\n🔰 `Título` 10.000.000<:estrelinha:831161441847345202>MPoints'
             },
             {
                 name: 'Itens Coletaveis',
-                value: '🍤 `Camarões` - Baú do Tesouro (Pesca)\n🐟 `Peixes` - Baú do Tesouro (Pesca)\n🪵 `Madeira` - Florestamento\n🦴 `Ossos` Mineração\n🪨 `Minérios` - Mineração\n💎 `Diamantes` - Mineração'
+                value: '🍤 `Camarões` - Baú do Tesouro (Pesca)\n🐟 `Peixes` - Baú do Tesouro (Pesca)\n🪵 `Madeira` - Florestamento\n🍎 `Maça` - Florestamento\n🦴 `Ossos` Mineração\n🪨 `Minérios` - Mineração\n💎 `Diamantes` - Mineração'
             }
         )
-        .setFooter(`${prefix}comprar Item`)
+        .setFooter(`${prefix}buy Item`)
     return message.inlineReply(loja)
 }

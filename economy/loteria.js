415b480d176971ad7058e8736dcbb53805e50242
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-04-17 19:54:16 -0300

    Create loteria.js

---
+++ economy/loteria.js
@@ -0,0 +1,42 @@
+const db = require('quick.db')
+const ms = require('parse-ms')
+const Discord = require('discord.js')
+
+exports.run = async (client, message, args) => {
+
+    if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) {
+      const adm = new Discord.MessageEmbed()
+        .setColor('#FF0000')
+        .setTitle('Eu preciso da permissão "Gerenciar Mensagens" para utilizar esta função.')
+      return message.inlineReply(adm)
+    }
+
+    let timeout1 = 6140000
+    let author1 = await db.fetch(`pego_${message.author.id}`)
+
+    if (author1 !== null && timeout1 - (Date.now() - author1) > 0) {
+        let time = ms(timeout1 - (Date.now() - author1))
+
+        const presomax = new Discord.MessageEmbed()
+            .setColor('#FF0000')
+            .setTitle('🚨 Você está em prisão máxima!')
+            .setDescription('`Liberdade em: ' + `${time.minutes}` + 'm e ' + `${time.seconds}` + 's`')
+
+        return message.inlineReply(presomax)
+    } else {
+
+        let timeout = 1728000
+        let author = await db.fetch(`lotery_${message.author.id}`)
+
+        if (author !== null && timeout - (Date.now() - author) > 0) {
+            let time = ms(timeout - (Date.now() - author))
+            return message.inlineReply(`Você pode jogar novamente em ${time.minutes}m e ${time.seconds}s`)
+        } else {
+            var amount = Math.floor(Math.random() * 1000) + 1
+            db.add(`money_${message.author.id}`, amount)
+            db.set(`lotery_${message.author.id}`, Date.now())
+
+            await message.inlineReply(`Você jogou e ganhou ${amount} <:StarPoint:766794021128765469>MPoints.`)
+        }
+    }
+}
